4d6a9f5706f2c5c17757ec87d7f2cce4f46fbeb4
Daijiro Chiba <dchiba@caravandazzle02mac.local>
2017-02-20 11:23:55 +0900

    シート名のファイルを出力するように

--- main.go
+++ main.go
@@ -1,40 +1,74 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 
 	"github.com/tealeg/xlsx"
 )
 
-func Usage() {
-	fmt.Println(`
-Usage: excel-exporter FILE.xlsx > OUTPUT.csv
-`)
+var (
+	sheetNames = flag.String("s", "", "カンマ区切りのシート名")
+)
+
+func usage() {
+	fmt.Printf("Usage:\n")
+	fmt.Printf("\texcel-exporter [Flags] FILE.xlsx\n")
+	fmt.Printf("Output:\n")
+	fmt.Printf("\tSheetName.txt[, SheetName2.txt]\n")
+	fmt.Printf("Flags:\n")
+	flag.PrintDefaults()
 }
 
 func main() {
-	flag.Usage = Usage
+	flag.Usage = usage
 	flag.Parse()
 	excelFileName := flag.Arg(0)
 	if excelFileName == "" {
-		Usage()
+		usage()
 		os.Exit(2)
 	}
+	run(excelFileName, strings.Split(*sheetNames, ","))
+}
+func run(excelFileName string, sheets []string) {
 	xlFile, err := xlsx.OpenFile(excelFileName)
 	if err != nil {
 		panic(err)
 	}
 	for _, sheet := range xlFile.Sheets {
-		for _, row := range sheet.Rows {
-			list := []string{}
-			for _, cell := range row.Cells {
-				text, _ := cell.String()
-				list = append(list, text)
+		if contains(sheets, sheet.Name) {
+			b := new(bytes.Buffer)
+			for _, row := range sheet.Rows {
+				list := []string{}
+				for _, cell := range row.Cells {
+					text, _ := cell.String()
+					list = append(list, text)
+				}
+				_, err := b.WriteString(strings.Join(list, ",") + "\r\n")
+				if err != nil {
+					panic(err)
+				}
 			}
-			fmt.Printf("%s\r\n", strings.Join(list, ","))
+			err := ioutil.WriteFile(sheet.Name+".txt", b.Bytes(), 0644)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
+}
+
+func contains(list []string, n string) bool {
+	if *sheetNames == "" {
+		return true
+	}
+	for _, s := range list {
+		if s == n {
+			return true
+		}
+	}
+	return false
 }
